1193f49d8eff731ee836b9d2befc8159416f94b1
RafaelMacharete <rafaelmacharete00@gmail.com>
2024-12-08 21:43:01 -0300

    adm funcionando

---
+++ scripts/database.js
@@ -0,0 +1,368 @@
+export const database = [ /* "Banco de dados" de produtos comercializados no site */
+    {
+        codigoProduto:1,
+        tituloProduto:"Placa de Vídeo RTX 4090 OC Asus NVIDIA ROG Strix, 24 GB GDDR6X, ARGB, DLSS, Ray Tracing - ROG-STRIX-RTX4090-O24G-GAMING", 
+        preco: 13209.99, 
+        descricao:"Desempenho e eficiência de energia até 2x, Núcleos tensores de 4ª geração: desempenho de IA até 2X, Núcleos RT de 3ª Geração: Desempenho de rastreamento de raios até 2X, Ventiladores de tecnologia axial ampliados para 23% mais fluxo de ar.",
+        imagemProduto: {
+            img1:"./images/asus.webp",
+            img2:"./images/asus2.webp",
+            img3:"./images/asus3.webp",
+            img4:"./images/asus4.webp",
+
+        },
+        categoriaProduto: 'Hardware',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:2,
+        tituloProduto:"Gabinete Gamer NZXT H9 Elite, Mid Tower, RGB, ATX, Lateral, Frontal e Topo em Vidro, Branco - CM-H91EW-01", 
+        preco: 339.00, 
+        descricao:"O H9 Elite foi projetado para mostrar todos os ângulos de sua construção com um painel superior de vidro temperado e um RGB e controlador de ventoinha integrados. O design exclusivo de vidro envolvente com um layout de câmara dupla é a vitrine perfeita para componentes premium.",
+        imagemProduto: {
+            img1:"./images/gabinete1.webp",
+            img2:"./images/gabinete2.webp",
+            img3:"./images/gabinete3.webp",
+            img4:"./images/gabinete4.webp",
+    },
+        categoriaProduto: 'Gabinete',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:3,
+        tituloProduto:"Monitor Gamer Curvo Samsung Odyssey G5 34, WQHD, Ultrawide, 165Hz, 1ms, DisplayPort e HDMI, Freesync Premium, Preto - LC34G55TWWLMZD", 
+        preco: 3099.99, 
+        descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+        imagemProduto: {
+            img1:"./images/monitors1.webp",
+            img2:"./images/monitors2.webp",
+            img3:"./images/monitors3.webp",
+            img4:"./images/monitors1.webp",
+    },
+        categoriaProduto: 'Monitor',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:4,
+        tituloProduto:"Monitor Gamer Curvo LG UltraGear LG 34, UltraWide, 160Hz, WQHD, 1ms, DisplayPort e HDMI, AMD FreeSync Premium, HDR10, 99% sRGB - 34GP63A-B", 
+        preco: 2039.99, 
+        descricao:"Se você está em busca de uma experiência imersiva para diversos tipos de jogos, como estratégia, FPS e corrida, este monitor é perfeito para você. Com a resolução QHD UltraWide (3440x1440), você ganha 30% a mais de área de tela, garantindo que nenhum detalhe do jogo passe despercebido. Além disso, o modelo conta com som estéreo 7W com MaxxAudio, oferecendo uma qualidade de som envolvente. A proporção de tela 21:9 também aumenta sua produtividade, permitindo que você acesse dois conteúdos simultaneamente na mesma tela, ideal para jogar e transmitir ou trabalhar ao mesmo tempo.",
+        imagemProduto: {
+            img1:"./images/monitor1.webp",
+            img2:"./images/monitor2.webp",
+            img3:"./images/monitor3.webp",
+            img4:"./images/monitor1.webp",
+
+        },
+        categoriaProduto: 'Monitor',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true
+    },
+    {
+        codigoProduto:5,
+        tituloProduto:"Ssd Rise Mode Gamer Line 2tb - RM-SSD-2TB",
+        preco: 116.99, 
+        descricao:"O SSD Rise Mode Gamer Line é um SSD SATA III 6 Gb/s de alto desempenho que oferece velocidade de leitura sequencial de até 535 MB/s e velocidade de gravação sequencial de 435 MB/s. ",
+        imagemProduto: {
+            img1:"./images/ssd_1.webp",
+            img2:"./images/ssd_2.webp",
+            img3:"./images/ssd_3.webp",
+            img4:"./images/ssd.webp",
+
+        },
+        categoriaProduto: 'Monitor',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: false
+    },
+    {
+        codigoProduto:6,
+        tituloProduto:"Teclado Mecânico Gamer Razer BlackWidow V3 Tenkeyless, Chroma Razer RGB, Switch Yellow, US - RZ03-03491800-R3M1", 
+        preco: 989.99, 
+        descricao:"Sinta a diferença Desempenho fatal em um formato mais enxuto – conheça o Razer BlackWidow V3 Tenkeyless. Dando continuidade a um legado icônico, este teclado para jogos compacto está armado com nossos mundialmente renomados Switches Mecânicos Razer e equipado com Razer Chroma RGB, para oferecer o nível de precisão e de personalização mais adorado pelos gamers de todo o mundo. Switches mecânicos amarelos Razer O switch mais rápido e mais silencioso. O melhor para toques de disparo rápido e jogos de ritmo veloz, o Switch Mecânico Razer Amarelo tem um acionamento ultra-veloz de apenas 1,2 mm, que permite que você pressione as teclas várias vezes o mais rápido possível. Equipado com Razer Chroma RGB para uma iluminação personalizável e uma maior imersão no jogo Vida útil de 80 milhões de ativações por tecla para uma longa durabilidade Formato compacto para um setup mais dinâmico Ooções de roteamento do cabo para uma fácil gestão do cabo Teclas totalmente programáveis para perfis, atalhos e macros personalizados",
+        imagemProduto: {
+            img1:"./images/teclado1.webp",
+            img2:"./images/teclado2.webp",
+            img3:"./images/teclado3.webp",
+            img4:"./images/teclado1.jpg",
+
+        },
+        categoriaProduto: 'Periférico',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:7,
+        tituloProduto:"SSD 1 TB Kingston NV2, M.2 2280 PCIe, NVMe, Leitura: 3500 MB/s e Gravação: 2100 MB/s - SNV2S/1000G", 
+        preco: 369.99, 
+        descricao:"O NV2 PCIe 4.0 NVMe SSD da Kingston é uma solução substancial de armazenamento de última geração alimentada por um controlador Gen 4x4 NVMe. O NV2 oferece velocidades de leitura/gravação de até 3.500/2.100 MB/s com menores requisitos de energia e menor aquecimento para ajudar a otimizar o desempenho do seu sistema e agregar valor sem sacrifícios.",
+        imagemProduto: {
+            img1:"./images/ssd1.webp",
+            img2:"./images/ssd2.webp",
+            img3:"./images/ssd3.webp",
+            img4:"./images/ssd4.webp",
+
+        },
+        categoriaProduto: 'Hardware',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:8,
+        tituloProduto:"Water Cooler Rise Mode, ARGB, 240mm, AMD/Intel, Preto - RM-WCB-04-ARGB", 
+        preco: 229.99, 
+        descricao:"A escolha perfeita para entusiastas em jogos que desejam manter seus PCs resfriados enquanto exibem um show de leds. Este water cooler comporta processadores com TDP de 220W e possui dois fans de 120mm que operam a velocidades entre 1200-2000 RPM, garantindo o resfriamento eficaz do seu sistema, mesmo durante os jogos mais intensos.",
+        imagemProduto: {
+            img1:"./images/cooler1.webp",
+            img2:"./images/cooler2.webp",
+            img3:"./images/cooler3.webp",
+            img4:"./images/cooler.webp",
+
+        },
+        categoriaProduto: 'Ventilação',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:9,
+        tituloProduto:"Memória RAM Rise Mode Diamond 8GB, 3200MHz, DDR4, CL15, Branco - RM-D4-8G-3200D", 
+        preco: 135.89, 
+        descricao:"Com velocidades de 3200MHz, a memória RAM Diamond oferece um desempenho excepcional para suas necessidades.",
+        imagemProduto: {
+            img1:"./images/memoria1.webp",
+            img2:"./images/memoria2.webp",
+            img3:"./images/memoria3.webp",
+            img4:"./images/memorearam.webp",
+
+        },
+        categoriaProduto: 'Hardware',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true
+    },
+    {
+        codigoProduto:10,
+        tituloProduto:"Monitor Gamer Curvo Samsung Odyssey 49 OLED, Super Ultrawide, 240Hz, 0.03ms, DisplayPort, HDMI, Freesync Premium, Prata - LS49CG930SLMZD,",
+        preco: 7899.00, 
+        descricao:"Tecnologia Samsung OLED Desfrute de gráficos deslumbrantes em seus jogos com o Odyssey OLED G9. A exclusiva Tecnologia de Pontos Quânticos proporciona imagens com contraste puro, além de cores mais brilhantes e vibrantes.Experiência Ultra Imersiva tela Super Ultra-Wide, equivalente a dois monitores QHD lado a lado, permite que você capte todos os detalhes do seu jogo com máxima nitidez. A curvatura de 1800R da tela coloca você no centro da ação, proporcionando uma imersão incomparável.",
+        imagemProduto: {
+            img1:"./images/monitor_1.webp",
+            img2:"./images/monitor_2.webp",
+            img3:"./images/monitor_3.webp",
+            img4:"./images/monitor_4.webp",
+
+        },
+        categoriaProduto: 'Monitor',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true
+    },
+    {
+        codigoProduto:11,
+        tituloProduto:"Cadeira Gamer KBM! GAMING Tempest CG500 Preta Com Almofadas, Descanso Para Pernas Retrátil, Reclinável - KGCG500PT",
+        preco: 928.79, 
+        descricao:"Conheça a cadeira KBM! GAMING Tempest CG500! Perfeita para completar seu setup. Feito com espuma de altíssima densidade PU, a cadeira KBM! GAMING Tempest CG500 possui descanso para os pés, ideal para aquela pausa entre um jogo e outro! Além do design moderno e robusto, a cadeira proporciona ótima performance nas partidas devido a sensação de conforto que é convertida em foco e concentração, tudo que um gamer de verdade precisa para ser o melhorT",
+        imagemProduto: {
+            img1:"./images/cadeira_1.webp",
+            img2:"./images/cadeira_2.webp",
+            img3:"./images/cadeira_3.webp",
+            img4:"./images/cadeira_4.webp",
+
+        },
+        categoriaProduto: 'Cadeira',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true
+    },
+    {
+        codigoProduto:12,
+        tituloProduto:"Fonte KBM! GAMING FN750, 750W, 80 Plus Gold, Cybenetics Gold, Full Modular, PFC Ativo, Com Cabo, Preto - KGFN75075PT",
+        preco: 419.99, 
+        descricao:"A performance dos componentes estão diretamente ligados ao desempenho de sua fonte, e na Fonte 750W KBM! GAMING a certificação 80 PLUS Gold tem mais de 90% de eficiência, garantindo menor consumo e melhor eficiência no uso de energia.",
+        imagemProduto: {
+            img1:"./images/fonte1.webp",
+            img2:"./images/fonte2.webp",
+            img3:"./images/fonte3.webp",
+            img4:"./images/fonte4.webp",
+
+        },
+        categoriaProduto: 'Fonte_Alimentação',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true
+    },
+    {
+        codigoProduto:13,
+        tituloProduto:"Microfone Gamer HyperX QuadCast S Podcast, Antivibração, LED RGB, USB, Compatível com PC, PS4 e Mac, Preto - 4P5P7AA",
+        preco: 579.00, 
+        descricao:"O HyperX QuadCast é o microfone independente ideal com todos os recursos para o streamer ou podcaster que procura um microfone com som de qualidade. O QuadCast vem com sua própria base 'shock mount' antivibração para ajudar a reduzir os ruídos da vida diária e um filtro integrado para abafar sons percussivos desagradáveis. Saiba instantaneamente o status do seu microfone com o indicador em LED e simplesmente toque para colocá-lo em mudo para evitar acidentes inconvenientes na transmissão.",
+        imagemProduto: {
+            img1:"./images/microfone1.webp",
+            img2:"./images/microfone2.webp",
+            img3:"./images/microfone3.webp",
+            img4:"./images/microfone4.webp",
+
+        },
+        categoriaProduto: 'Microfone',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true
+    },
+    {
+        codigoProduto:14,
+        tituloProduto:"SSD SATA WD Green, 240GB, 2.5, Leitura: 545MB/s e Gravação: 430MB/s, Preto - WDS240G3G0A",
+        preco: 134.99, 
+        descricao:"Com o aumento de desempenho de um SSD SATA WD Green, você pode navegar na web, jogar um jogo ou simplesmente iniciar seu sistema rapidamente.",
+        imagemProduto: {
+            img1:"./images/ssds1.webp",
+            img2:"./images/ssds2.webp",
+            img3:"./images/ssds2.webp",
+            img4:"./images/ssds1.webp",
+
+        },
+        categoriaProduto: 'Microfone',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true
+    },
+    {
+        codigoProduto:15,
+        tituloProduto:"Placa Mãe Asus Prime H510M-A, Intel Socket LGA1200, microATX, DDR4",
+        preco: 499.99, 
+        descricao:"A série Prime H510 foi projetada com vários dissipadores de calor integrado e uma variedade de conectores híbridos para garantir que seu equipamento permaneça refrigerado e estável sob cargas de trabalho intensas.",
+        imagemProduto: {
+            img1:"./images/placamae1.webp",
+            img2:"./images/placamae2.webp",
+            img3:"./images/placamae3.webp",
+            img4:"./images/placamae4.webp",
+
+        },
+        categoriaProduto: 'Microfone',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:16,
+        tituloProduto:"Placa Mãe Asus Prime H510M-A, Intel Socket LGA1200, microATX, DDR4",
+        preco: 1439.99, 
+        descricao:"Desfrute de velocidades supersônicas com 8 núcleos e 16 threads de processamento, prontos para enfrentar os títulos mais desafiadores. Frequência base de 3,0GHz e boost dinâmico de até 4,1GHz para eliminar qualquer engasgo.",        
+        imagemProduto: {
+            img1:"./images/processador1.webp",
+            img2:"./images/processador2.webp",
+            img3:"./images/processador3.webp",
+            img4:"./images/processador4.webp",
+
+        },
+        categoriaProduto: 'Microfone',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:17,
+        tituloProduto:"Teclado Mecânico Gamer KBM! GAMING TG600, Branco, 60% e ABNT2, RGB, Switch Gateron Red - KGTG600BRVR",
+        preco: 209.99, 
+        descricao:"Switches mecânicos Gateron Red, com resposta instantânea e precisão para jogos competitivos. O Switch Red possui feedback tátil com sensação de maciez, de acionamento silencioso e durabilidade de mais de 50 milhões de cliques sem perder a qualidade.", 
+        imagemProduto: {
+            img1:"./images/teclado_1.webp",
+            img2:"./images/teclado_2.webp",
+            img3:"./images/teclado_3.webp",
+            img4:"./images/teclado_4.webp",
+
+        },
+        categoriaProduto: 'Microfone',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:18,
+        tituloProduto:"Headset Gamer HyperX Cloud Stinger 2, Drivers 40mm, para Xbox, Branco - 6H9B7AA",
+        preco: 249.99, 
+        descricao:"O CloudX Stinger Core foi testado e aprovado pela Microsoft e se conecta diretamente aos controles do Xbox One e Xbox Series X|S com uma porta de 3,5 mm.", 
+        imagemProduto: {
+            img1:"./images/headset1.webp",
+            img2:"./images/headset2.webp",
+            img3:"./images/headset3.webp",
+            img4:"./images/headset4.webp",
+
+        },
+        categoriaProduto: 'Microfone',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:19,
+        tituloProduto:"Gabinete Gamer Rise Mode Glass 06X, Mid Tower, Lateral em Vidro Fumê e Frontal em Vidro Temperado, Preto - RM-CA-06X-FB",
+        preco: 169.99, 
+        descricao:"Não contém Fans (0 Fans).", 
+        imagemProduto: {
+            img1:"./images/gabinete_1.webp",
+            img2:"./images/gabinete_2.webp",
+            img3:"./images/gabinete_3.webp",
+            img4:"./images/gabinete_4.webp",
+
+        },
+        categoriaProduto: 'Microfone',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:20,
+        tituloProduto:"Headset com fio USB Logitech H390 com Almofadas, Controles de Áudio Integrado e Microfone com Redução de Ruído - 981-000014",
+        preco: 169.99, 
+        descricao:"O arco de cabeça ajustável com fones acolchoados giratórios  oferece horas de conforto. E também é fácil de limpar.", 
+        imagemProduto: {
+            img1:"./images/headset_1.webp",
+            img2:"./images/headset_2.webp",
+            img3:"./images/headset_3.webp",
+            img4:"./images/headset_4.webp",
+
+        },
+        categoriaProduto: 'Microfone',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:21,
+        tituloProduto:"Suporte Ergonômico Multiarticulado de Mesa para Monitor 17 a 35 ELG F98HDMI, Pistão à Gás, VESA, Grafite - F98HDMI",
+        preco: 154.90, 
+        descricao:"Um suporte Ergonômico perfeitamente adequado para ajustar seu monitor.", 
+        imagemProduto: {
+            img1:"./images/suporte1.webp",
+            img2:"./images/suporte2.webp",
+            img3:"./images/suporte3.webp",
+            img4:"./images/suporte4.webp",
+
+        },
+        categoriaProduto: 'Microfone',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:22,
+        tituloProduto:"Kit Gamer Redragon Teclado Mecânico Kumara, RGB, Switch Outemu Blue, PT, Branco + Mouse Cobra M711, Chroma, 10000DPI, Branco - S118W ",
+        preco: 224.99, 
+        descricao:"Um kit de mouse e teclado para atender e superar todas as suas expectitivas durante sua experiência, tanto nos jogos, como em trabalhos.", 
+        imagemProduto: {
+            img1:"./images/kit1.webp",
+            img2:"./images/kit2.webp",
+            img3:"./images/kit3.webp",
+            img4:"./images/kit4.webp",
+
+        },
+        categoriaProduto: 'Microfone',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:5090,
+        tituloProduto:"Kit Gamer Redragon Teclado Mecânico Kumara, RGB, Switch Outemu Blue, PT, Branco + Mouse Cobra M711, Chroma, 10000DPI, Branco - S118W ",
+        preco: 999999.1, 
+        descricao:"Um kit de mouse e teclado para atender e superar todas as suas expectitivas durante sua experiência, tanto nos jogos, como em trabalhos.", 
+        imagemProduto: {
+            img1:"./images/5090_meme.webp",
+            img2:"./images/5090_meme_2.webp",
+            img3:"./images/5090_meme.webp",
+            img4:"./images/5090_meme_2.webp",
+
+        },
+        categoriaProduto: 'Microfone',
+        classificacaoProduto:"Novidades",
+        exibirHome: 0
+    },
+]
